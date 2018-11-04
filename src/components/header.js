aeccd5a21dbd79662301f308c51adf162d3d80f0
Ida-Maria Isaksson <idamaria.isaksson@screeninteraction.com>
2018-11-04 09:26:34 +0100

    Styled header

--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,19 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Navigation from './navigation'
+import logo from "../images/hjortron.png"
 import './header.css'
 
 const Header = ({ siteTitle }) => (
-      <div className = { "header" }>
-        <div className= { "wrapper" } >
-          <h1>
-            <Link to="/"> { siteTitle } </Link>
-          </h1>
-
-          <Navigation></Navigation>
-
-        </div>
+  <div className = { "header" }>
+      <div className = { "logo "}>
+        <img src={logo} alt="Logo" />
       </div>
+      <h1>
+        <Link to="/"> { siteTitle } </Link>
+      </h1>
+      <Navigation></Navigation>
+  </div>
 )
 
 export default Header
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,19 +5,14 @@ import './navigation.css'
 
 
 const Navigation = () => (
-    <div className = { "header" }>
-        <div className= { "wrapper" }>
-          <ul>
-              <li><Link to="/">Start</Link></li>
-              <li><Link to="/vitaminer/">Information</Link></li>
-              <li><Link to="/vitaminer/">Återhämtning</Link></li>
-              <li><Link to="/vitaminer/">Vitaminer</Link></li>
-              <li><Link to="/vitaminer/">Termer</Link></li>
-              <li><Link to="/vitaminer/">Podcasts och böcker</Link></li>
-            </ul>
-
-        </div>
-      </div>
+        <ul className = { "navigation "}>
+            <li><Link to="/">Start</Link></li>
+            <li><Link to="/vitaminer/">Information</Link></li>
+            <li><Link to="/vitaminer/">Återhämtning</Link></li>
+            <li><Link to="/vitaminer/">Vitaminer</Link></li>
+            <li><Link to="/vitaminer/">Termer</Link></li>
+            <li><Link to="/vitaminer/">Podcasts och böcker</Link></li>
+        </ul>
 )
 
 export default Navigation
