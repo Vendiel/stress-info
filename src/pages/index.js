c16f9d696e86df576a45bf519e522910d8c01126
Ida-Maria Isaksson <vendiel@hotmail.com>
2019-03-10 09:14:13 +0100

    Updated text and intro img

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
       <div class="wrapper">
         <div class="introduction">
           <h1>Stress och utmattning</h1>
-          <p>Välkommen till den här sajten som fokuserar på stress och utmattning. Jag har skapat denna sajt då jag har gjort en resa i ämnet och ville ha ett ställe att samla all information jag samlat på mig via podcasts, böcker, andra sajter, osv - så jag inte glömmer bort den. För det är ju en del av kruxet med det här tillståndet - man har inte världens bästa minne.</p>
+          <p>Välkommen till den här sajten som fokuserar på stress och utmattning. Skälet till att denna sajt skapades var för att ha ett ställe att samla all information jag samlat på mig via podcasts, böcker, andra sajter, osv. Ett ställe att gå tillbaka till när man behöver friska upp minnet.</p>
         </div>
       </div>
     </div>
@@ -19,8 +19,8 @@ const IndexPage = () => (
       <div class="wrapper">
         <div class="col-9">
           <h2>Mer om varför</h2>
-          <p>Förutom att jag vill ha ett ställe att samla all information jag hittar så hoppas jag att det kanske kan hjälpa någon annan som hamnat i en liknande situation. För precis som en del andra jag känner som har/haft problem med långvarig stress så vill jag hjälpa - enda skillnaden är att medan de utbildar sig till stresscoacher, terapeuter etc så skapar jag en sajt. </p>
-          <p>Ett annat skäl till den här sajten är att jag hittat ett hål i internet. I början av min sjukskrivning så googlade jag på utmattningssyndrom och stress och hittade massa artiklar, bloggar osv i ämnet men hittade ingen sida som gav mig en fullständig bild. Från början försökte jag skapa den bilden med hjälp av en mindmap (bilden nedan visar en bit av den) men den bara växte och i slutändan saknade jag den djupgående informationen. Så med den här sidan tänkte jag bit för bit försöka täcka så mycket jag kan och orkar.</p>
+          <p>Förutom att jag vill ha ett ställe att samla all information jag hittar så hoppas jag att det kanske kan hjälpa någon annan som hamnat i en liknande situation. För precis som en del andra jag känner som har/haft problem med långvarig stress så vill jag hjälpa - enda skillnaden är att medan de utbildar sig till stresscoacher, terapeuter etc så skapar jag en sajt.</p>
+          <p>Ett annat skäl till den här sajten är att jag hittat ett hål i internet. I början av min sjukskrivning så googlade jag på utmattningssyndrom och stress och hittade massa artiklar, bloggar, osv i ämnet men hittade ingen sida som gav mig en fullständig bild. Från början försökte jag skapa den bilden med hjälp av en mindmap (bilden nedan visar en bit av den) men den bara växte och i slutändan saknade jag ändå den djupgående informationen. Så med den här sidan tänkte jag bit för bit försöka täcka så mycket jag kan och orkar.</p>
           <img src={mindmap} class="mindmap" alt="Utmattningssyndrom mindmap" />
           <p>Förutom stress och utmattning så kommer den här sajten även att komma i kontakt med närliggande områden som oro och ångest. Jag anar också att det kommer bli en hel del information också om hur vi fungerar som människor, då jag gillar beteenden.</p>
         </div>
--- src/pages/information.js
+++ src/pages/information.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
       <div class="wrapper">
         <div class="introduction">
           <h1>Information</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+          <p>Här hittar du information kring vad stress är och vad som händer i kroppen vid både kort och långvarig stress.</p>
         </div>
       </div>
     </div>
@@ -18,8 +18,7 @@ const IndexPage = () => (
       <div class="wrapper">
         <div class="col-9">
           <h2>Vad är stress?</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-          <h2>Varför upplever vi stress?</h2>
+          <p>Enligt wikipedia så är stress en fysiologisk-hormonella anpassningsreaktioner i kroppens organsystem som utlöses av fysiska och mentala påfrestningar <sup><a href="#references">[1]</a></sup>. Ingibjörg Jonsdottir beskriver det som biologiska och fysiologiska förberedelser i kroppen när man står inför en utmaning <sup><a href="#references">[2]</a></sup>.  </p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
           <h2>Långvarig stress</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
@@ -35,8 +34,8 @@ const IndexPage = () => (
       <div class="wrapper">
         <div class="col-9">
           <h2>Refenser</h2>
-          <ul class="references">
-            <li>Referens 1</li>
+          <ul class="references" name="references">
+            <li>1. Wikipedia. <a href="https://sv.wikipedia.org/wiki/Stress">https://sv.wikipedia.org/wiki/Stress</a>. Information hämtad 2019-01-05.</li>
           </ul>
         </div>
       </div>
